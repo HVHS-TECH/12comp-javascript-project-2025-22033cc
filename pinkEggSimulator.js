b9ceb8cafc23aba19dc4538f732ff8cb4d8dd5a7
22033cc <22033cc@hvhs.school.nz>
2025-03-13 22:00:46 +0000

    No man should have all that power

--- pinkEggSimulator.js
+++ pinkEggSimulator.js
@@ -1,25 +1,38 @@
 console.log("hello! Welcome to my game")
     const canvasWidth = 500;
     const canvasHeight = 700;
-    const wasd =['w','a','s','d','k','l'];
+    const letterKeys =['w','a','s','d','k','l'];
     const arrowKeys=["up","left","down","right","z","x"];
     const pinkEggSize =20;
-    
+    const pinkEggStartPostion =200;
+    const enemyStartPosition= 50;
+    var bulletOutputSpeed = 10;
     var pinkEggSpeed =2;
-    var controls = wasd;
+    var controls = letterKeys;
     var gameState = 'start';
-
+    var i=0;
+    var bulletPower = 100;
 /***********************************
  * set up
  ***********************************/
 function setup(){
     cnv = new Canvas ("5:7");//(canvasWidth,canvasHeight);
-    pinkEgg = new Sprite(pinkEggSize,pinkEggSize,pinkEggSize,'k');
+    pinkEgg = new Sprite(pinkEggStartPostion,pinkEggStartPostion,pinkEggSize,'k');
     bulletGroup = new Group();
+    whiteEggGang = new Group();
     console.log(controls[4]);
+    world.gravity.y=1;
 }
 function draw(){
+    console.log("powerleft"+bulletPower)
+    if(frameCount%100==0){
+        i=i+50;
+        console.log("an egg is born!"+i);
+        whiteEgg =  new Sprite(i,50,pinkEggSize,'d');
+        whiteEggGang.add(whiteEgg);
+    }
 
+    /*
     if (gameState='start'){
     background("#FF69B4")
     startScreen()
@@ -27,13 +40,24 @@ function draw(){
         gameState='playing';
         console.log("cranberry juice"+gameState);
     }
-    }
-     if (gameState='playing'){
+    } else if (gameState='playing')*/
     
     background('grey')
     pinkEggControls()
 
+    bulletGroup.collides(whiteEggGang,enemyHitBullet)
+    if (frameCount%bulletOutputSpeed*2&&100<bulletPower>0){
+        bulletPower=bulletPower+1;
+    }
+
 }
+function enemyHitBullet(_bullette,_eggo){
+    _bullette.color ="green";
+    _eggo.color ="red";
+    _bullette.remove();
+    console.log("BULLET REMOVED!");
+    _eggo.remove();
+    console.log("EGG REMOVED");
 }
 
 function startScreen(){
@@ -63,8 +87,10 @@ function pinkEggControls(){
         yPress = yPress+pinkEggSpeed;
         console.log("down")
     };
+ 
 
-    if(kb.pressing(controls[4])&&frameCount%10==0){
+    if(kb.pressing(controls[4])&&frameCount%bulletOutputSpeed==0&&bulletPower>0){
+        bulletPower=bulletPower-5;
         shootBulletPink();
     }
             pinkEgg.vel.x = xPress;
